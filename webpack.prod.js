39b89d450e6ba666923b9c955c2b56ef6c13e8a1
larturi <lea.arturi@gmail.com>
2020-05-19 15:48:52 -0300

    Webpack a Prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,28 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
  
-    mode: 'development',
+    mode: 'production',
     optimization: {
         minimizer: [ new OptimizeCssAssetsPlugin() ]
     },
+    output: {
+       filename: 'main.[contentHash].js'
+    },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'                
+                ]
+            },
             {
                 test: /\.css$/,
                 exclude: /styles\.css$/,
@@ -53,14 +65,16 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].[contentHash].css',
             ignoreOrder: false
         }),
         new CopyPlugin({
             patterns: [
                 { from: 'src/assets/images', to: 'assets/images' }
               ],
-        })
+        }),
+        new MinifyPlugin(),
+        new CleanWebpackPlugin()
     ]
  
 };
